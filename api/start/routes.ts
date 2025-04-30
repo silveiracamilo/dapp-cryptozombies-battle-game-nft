6883dd1db0b34e556e4bd1f559d9122cf69415f5
Camilo da Silveira <silveiracamilo@gmail.com>
2025-04-30 12:58:57 -0300

    feat(api): add routes /proof/root and /proof/:address

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -12,6 +12,9 @@ import env from '#start/env'
 import ZombieGenesMapper from '#services/zombie/ZombieGenesMapper';
 import ZombieImageService from '#services/zombie/ZombieImageService';
 import router from '@adonisjs/core/services/router'
+import fs from 'fs';
+import { MerkleTree }  from 'merkletreejs';
+import { keccak256 } from 'ethers';
 
 router.get('/', async () => {
   return 'Cryptozombies Battle API V1.0.0'
@@ -36,3 +39,37 @@ router.get('/zombie/image/:dna', async ({ params, response }) => {
   const image = await serviceImage.generateZombieImage(dna);
   response.type('png').stream(image);
 })
+
+router.get('/proof/root', async () => {
+  if (fs.existsSync('merkle_root.json')) {
+    const data = JSON.parse(fs.readFileSync('merkle_root.json', 'utf-8'));
+    return { root: data.root }
+  }
+
+  return { root: '' }
+})
+
+router.get('/proof/:address', async ({ params }) => {
+  if (fs.existsSync('merkle_root.json')) {
+    const { address } = params
+    const data = JSON.parse(fs.readFileSync('merkle_root.json', 'utf-8'));
+    const { approvedWallets, root } = data
+
+    const tree = new MerkleTree(
+      approvedWallets.map((addr: string) => keccak256(addr.toLowerCase())),
+      keccak256,
+      { sortPairs: true }
+    )
+
+    const leaf = keccak256(address.toLowerCase())
+    const proof = tree.getHexProof(leaf)
+
+    const isListed = approvedWallets.includes(address.toLowerCase())
+    const isValid = isListed && tree.verify(proof, leaf, root)
+
+    return { valid: isValid, proof, root }
+  }
+
+  return { valid: false, proof: [], root: '' }
+})
+
