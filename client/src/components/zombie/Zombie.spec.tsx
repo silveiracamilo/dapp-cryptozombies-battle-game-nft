ac9e34a9bfda7a23097ae356afe142802f979ccf
Camilo da Silveira <silveiracamilo@gmail.com>
2025-06-09 21:11:49 -0300

    test: updated test Zombie component

--- client/src/components/zombie/Zombie.spec.tsx
+++ client/src/components/zombie/Zombie.spec.tsx
@@ -1,13 +1,15 @@
 import { beforeAll, describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, RenderResult, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 import { Zombie } from './Zombie';
 
 describe("Zombie Component", () => {
     const dna = '6555859043139391';
+    const dnaCat = '3333637044366099';
+    let component: RenderResult;
 
     beforeAll(() => {
-        render(<Zombie dna={dna} />);
+        component = render(<Zombie dna={dna} />);
     });
     
     it("should render", () => {
@@ -27,6 +29,23 @@ describe("Zombie Component", () => {
         expect(screen.getByAltText('Coxa direita do zumbi')).toBeInTheDocument();
         expect(screen.getByAltText('Ombro direito do zumbi')).toBeInTheDocument();
         expect(screen.getByAltText('Torso do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Camiseta do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Ombro esquerdo do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Antebraço esquerdo do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Antebraço direito do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Mão esquerda do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Mão direita do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Cabeça do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Olho do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Boca do zumbi')).toBeInTheDocument();
+    });
+
+    it("should render all parts the zombie cat", () => {
+        component.rerender(<Zombie dna={dnaCat} />);
+
+        expect(screen.getByAltText('Ombro direito do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Torso do zumbi')).toBeInTheDocument();
+        expect(screen.getByAltText('Pernas de gato do zumbi')).toBeInTheDocument();
         expect(screen.getByAltText('Camiseta do zumbi')).toBeInTheDocument();
         expect(screen.getByAltText('Ombro esquerdo do zumbi')).toBeInTheDocument();
         expect(screen.getByAltText('Antebraço esquerdo do zumbi')).toBeInTheDocument();
